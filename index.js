0965bfb5856ceeeabee5ab4d971625f2d59521f9
sheldonwafula <wafulasheldon2@gmail.com>
2022-08-09 14:03:41 +0300

    greet

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ document.addEventListener('DOMContentLoaded',function(){
         }
     })
 })
+
+var today= new Date(); 
+var hourNow = today.getHours(); 
+var greeting; 
+if (hourNow > 18) { 
+greeting= 'Good evening!'; 
+}else if (hourNow > 12) { 
+greeting = ' Good afternoon!'; 
+}else if (hourNow > 0) { 
+greeting = 'Good morni ng!'; 
+}else { 
+greeting = 'Welcome! ' ;
+
+
+
+}
+document .write( ' <h2>' +greeting + ' </ h2> ');
