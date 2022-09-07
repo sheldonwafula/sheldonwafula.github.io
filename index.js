aff151e0d367e0970b92ac7ea988be476ceeb961
sheldonwafula <wafulasheldon2@gmail.com>
2022-09-07 14:10:26 +0300

    icon

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ greeting= 'Good evening!';
 }else if (hourNow > 12) { 
 greeting = ' Good afternoon!'; 
 }else if (hourNow > 0) { 
-greeting = 'Good morni ng!'; 
+greeting = 'Good morning!'; 
 }else { 
 greeting = 'Welcome! ' ;
 
